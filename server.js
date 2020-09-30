8ff2daf71906e258eb064bb26afe34480824dc5e
Beata Tomczak <tma.btty@gmail.com>
2020-09-30 23:13:06 +0200

    Implement messaging

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -10,21 +11,29 @@ const io = socketio(server)
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+const botName = 'InTouch Bot'
+
 // Run when client connects
 io.on('connection', socket => {
     // Welcome current user
-    socket.emit('message', 'Welcome to InTouch!')
+    socket.emit('message', formatMessage(botName, 'Welcome to InTouch!'))
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat')
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'))
 
     // Run when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
+        io.emit('message', formatMessage(botName,'A user has left the chat'))
+    })
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', formatMessage('USER', msg))
     })
 })
 
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
 
